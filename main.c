05e1c9d07b954c2806dba5e5cf70eb3c119da648
Bdp <breno.padilha@ufpr.br>
2026-05-24 18:47:15 -0300

    [mod] jogo em tela cheia

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main(){
 	al_init();																		//Faz a preparação de requisitos da biblioteca Allegro
 	al_install_keyboard();															//Habilita a entrada via teclado (eventos de teclado), no programa
 
+    al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+
 	ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);								//Cria o relógio do jogo; isso indica quantas atualizações serão realizadas por segundo (30, neste caso)
 	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();							//Cria a fila de eventos; todos os eventos (programação orientada a eventos) 
 	ALLEGRO_FONT* font = al_create_builtin_font();									//Carrega uma fonte padrão para escrever na tela (é bitmap, mas também suporta adicionar fontes ttf)
